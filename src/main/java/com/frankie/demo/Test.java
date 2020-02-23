0c8193816ead1692a7ceebe3fb0868de5710fd69
yycer <15900617310@163.com>
2020-02-23 21:03:31 +0800

    Play ClassLoader.

---
+++ src/main/java/com/frankie/demo/Test.java
@@ -0,0 +1,8 @@
+package com.frankie.demo;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/2/23 16:11
+ */
+public class Test {
+}
--- src/test/java/com/frankie/demo/ClassLoaderTest.java
+++ src/test/java/com/frankie/demo/ClassLoaderTest.java
@@ -1,7 +1,9 @@
 package com.frankie.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.misc.Launcher;
 
 /**
  * @author: Yao Frankie
@@ -11,10 +13,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ClassLoaderTest {
 
     @Test
-    void bootStrapPathTest(){
+    void threePathTest(){
         String bootPath = System.getProperty("sun.boot.class.path");
         String extPath  = System.getProperty("java.ext.dirs");
         String classPath = System.getProperty("java.class.path");
         System.out.println(1);
+    }
+
+    @Test
+    void classLoaderTest(){
+        ClassLoader cl = com.frankie.demo.Test.class.getClassLoader();
+//        System.out.println(cl); // sun.misc.Launcher$AppClassLoader@b4aac2
+        System.out.println(cl.getParent()); // sun.misc.Launcher$ExtClassLoader@18e2867
+        System.out.println(cl.getParent().getParent()); // null
+
+//        ClassLoader intCl = int.class.getClassLoader();
+//        System.out.println(intCl); // null
     }
 }
